7e0c9139621d487d42769aca05f3358325e44dab
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2019-10-16 16:10:44 +0300

    update

--- docs/build/p-qx3yv1sg.system.entry.js
+++
@@ -1,1 +0,0 @@
-System.register(["./p-bce1ce19.system.js"],(function(e){"use strict";var t,n;return{setters:[function(e){t=e.r;n=e.h}],execute:function(){var s=e("psk_themes",function(){function e(e){t(this,e)}e.prototype.render=function(){return n("div",{class:"psk-themes"},n("p",null,"App Themes"))};return e}())}}}));
---
+++ docs/build/p-xxnh3a2j.entry.js
@@ -0,0 +1,1 @@
+import{r,h as t}from"./p-f444b98a.js";const n=class{constructor(t){r(this,t)}render(){return t("app-menu",null)}static get style(){return""}};export{n as app_container};
