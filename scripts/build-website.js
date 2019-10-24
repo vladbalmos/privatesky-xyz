59bf6fe3e16a3a099b9c7fe32a8306baf3a7c696
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2019-10-24 23:06:47 +0300

    update siteamp at every website build

--- scripts/build-website.js
+++ scripts/build-website.js
@@ -1,7 +1,9 @@
-const pagesStructureUrls = ["./src/pages/pages-structure.json","./src/global/app-navigation-strucutre.json"];
 const fs = require("fs");
 const path = require("path");
 const child_process = require('child_process');
+const navigationPages = "./src/pages/pages-structure.json";
+const globalNavigation = "./src/global/app-navigation-structure.json";
+const websiteUrlBase = "https://privatesky.xyz/#";
 
 
 let copyRecursiveSync = function (src, dest) {
@@ -53,6 +55,98 @@ let buildWebsite = function (callback) {
   });
 };
 
+
+let buildSitemap = function () {
+
+
+  let readFileData = function (pageStructureUrl, callback) {
+    fs.readFile(path.resolve(pageStructureUrl), (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        callback(JSON.parse(data));
+      }
+    })
+
+  }
+
+  let fillOptionalProps = (navigationPages, pathPrefix) => {
+
+    navigationPages.forEach(page => {
+
+      if (!page.path) {
+        page.path = page.name;
+      }
+
+      if (pathPrefix) {
+        page.path = pathPrefix + "/" + page.path;
+      }
+
+
+      if (page.children) {
+        fillOptionalProps(page.children, page.path);
+      }
+    });
+  };
+  let siteMap = "";
+  let buildXMLTag = function (websitePages) {
+
+    websitePages.forEach(page => {
+      siteMap += `<url>
+        <loc>${websiteUrlBase}${page.path}</loc>
+      </url>`;
+
+      if (page.children) {
+        buildXMLTag(page.children)
+      }
+    });
+
+  };
+
+
+  readFileData(navigationPages, function (navigationPagesData) {
+    readFileData(globalNavigation, function (globalNavigation) {
+
+
+      fillOptionalProps(navigationPagesData);
+
+      let rootPages = globalNavigation.map(rootPage => rootPage.name);
+
+      navigationPagesData.forEach((page, index) => {
+        if (page.parent > -1) {
+          let parentIndex = rootPages.indexOf(page.parent);
+          if (parentIndex) {
+            if (!globalNavigation[parentIndex].children) {
+              globalNavigation[parentIndex].children = [];
+            }
+            page.path = globalNavigation[parentIndex].path + page.path;
+            globalNavigation[parentIndex].children.push(page);
+            navigationPagesData.splice(index, 1);
+          }
+        }
+      });
+
+      let allWebsitePages = navigationPagesData.concat(globalNavigation);
+      buildXMLTag(allWebsitePages);
+      siteMap = `<?xml version="1.0" encoding="UTF-8"?>
+                 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+                 ${siteMap}
+                 </urlset>`;
+      fs.writeFile("./docs/sitemap.xml", siteMap, function (err) {
+        if (err) {
+          console.log("An error occurred while generating the sitemap");
+        }
+        else {
+          console.log("Sitemap was successfully generated");
+        }
+
+      })
+    })
+  })
+
+};
+
 let makeWebsite = function () {
   buildWebsite(() => {
     console.log("Cleaning previous docs...");
@@ -60,31 +154,15 @@ let makeWebsite = function () {
     deleteFolderRecursive('./docs/build');
     console.log("Previous docs was successfully cleaned.");
     console.log("Copying current docs...");
-    copyRecursiveSync("./www/assets","./docs/assets");
-    copyRecursiveSync("./www/build","./docs/build");
-    copyRecursiveSync("./www/index.html","./docs/index.html");
+    copyRecursiveSync("./www/assets", "./docs/assets");
+    copyRecursiveSync("./www/build", "./docs/build");
+    copyRecursiveSync("./www/index.html", "./docs/index.html");
     console.log("Current docs was successfully copied.");
+    buildSitemap();
   });
 };
 
-let mergePagesStructure = function(){
-  pagesStructureUrls.forEach(pageStructureUrl=>{
-
-
-      fs.readFile(path.resolve(pageStructureUrl), (err, data) => {
-        if (err)
-          cb(err)
-        else
-          cb(null, JSON.parse(data))
-      })
-
-    let pageStructure = require(pageStructureUrl);
-  })
-}
-
-//makeWebsite();
-mergePagesStructure();
-
+makeWebsite();
 
 
 
