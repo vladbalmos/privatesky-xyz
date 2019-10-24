61618bc93558c4433fa80b59685e3e1390bf2a76
Paius Paraschiv-Catalin <p.cata95@gmail.com>
2019-10-24 16:14:23 +0300

    toc - wip

--- src/components/usages/page-not-found-usage/page-not-found-usage.tsx
+++ src/components/usages/page-not-found-usage/page-not-found-usage.tsx
@@ -8,26 +8,25 @@ import { Component, h } from "@stencil/core";
 export class NotFoundUsage {
     render() {
         return (
-            <psk-page title="psk-page-not-found">
-                <psk-tag>&lt;psk-page-not-found /&gt;</psk-tag>
+            <psk-page title="psk-page-not-found"
+                tableOfContentTitle="Page not found - Table Of Content">
+                <psk-tag>
+                    <psk-page-not-found />
+                </psk-tag>
 
                 <psk-description>
-                    <psk-card title="Description">
-                        <div class="card-body">
-                            <p>This component is intended to be used inside a router component, so when the user navigates to an unknown route, this component will be loaded.</p>
-                            <p>This component should be instantiated everywhere in application where there is a possibility to navigate to a wrong or unknown route.</p>
-                            <p>The component has the role to redirect the user to a valid route inside the application by using <code>basePath</code> or <code>urlDestination</code> properties.</p>
-                            <p>If none of these two properties are given, then a third property is checked: <code>pageRenderer</code></p>
-                            <p>The the order of the properties priority is the following:</p>
-                            <p>
-                                <ul>
-                                    <li><code>basePath</code></li>
-                                    <li><code>urlDestination</code> - if basePath is not given, this property will be used</li>
-                                    <li><code>pageRenderer</code> - if urlDestination is not given, pageRenderer will be used</li>
-                                </ul>
-                            </p>
-                        </div>
-                    </psk-card>
+                    <p>This component is intended to be used inside a router component, so when the user navigates to an unknown route, this component will be loaded.</p>
+                    <p>This component should be instantiated everywhere in application where there is a possibility to navigate to a wrong or unknown route.</p>
+                    <p>The component has the role to redirect the user to a valid route inside the application by using <code>basePath</code> or <code>urlDestination</code> properties.</p>
+                    <p>If none of these two properties are given, then a third property is checked: <code>pageRenderer</code></p>
+                    <p>The the order of the properties priority is the following:</p>
+                    <p>
+                        <ul>
+                            <li><code>basePath</code></li>
+                            <li><code>urlDestination</code> - if basePath is not given, this property will be used</li>
+                            <li><code>pageRenderer</code> - if urlDestination is not given, pageRenderer will be used</li>
+                        </ul>
+                    </p>
                 </psk-description>
 
                 <psk-chapter title="Supported properties">
@@ -45,7 +44,8 @@ export class NotFoundUsage {
                     <span class="nt">&lt;psk-page-not-found /&gt;</span>
                 </psk-chapter>
 
-                <psk-example exampleTitle="Usage Preview with the default pageRenderer">
+
+                <psk-example title="Usage Preview with the default pageRenderer">
                     <psk-page-not-found />
                 </psk-example>
             </psk-page>
