daa163146ddea3ce156bd770f37ea2564316e2fc
Paval Victor <pavalvictor98@yahoo.com>
2019-11-07 14:48:47 +0200

    update index.html

---
+++ docs/build/p-8p5dnv5c.entry.js
@@ -0,0 +1,1 @@
+import{r,h as n}from"./p-2972acc2.js";const s=class{constructor(n){r(this,n)}render(){return n("app-menu",null)}};export{s as app_container};
--- docs/build/p-w9tinoe6.entry.js
+++
@@ -1,1 +0,0 @@
-import{r,h as t}from"./p-2972acc2.js";const n=class{constructor(t){r(this,t)}render(){return t("app-menu",null)}static get style(){return""}};export{n as app_container};
