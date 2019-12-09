42ec81fcbe14ac16aa869218f05627442477daa5
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2019-12-09 18:58:52 +0200

    fix

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
 import DefaultController from "./controllers/DefaultController.js";
+import Controller from "./controllers/Controller.js";
 
 document.addEventListener("controllerFactoryIsReady", (e) => {
     let ControllerFactory = e.detail;
+    ControllerFactory.registerController("Controller", Controller);
     ControllerFactory.registerController("DefaultController", DefaultController);
 });
