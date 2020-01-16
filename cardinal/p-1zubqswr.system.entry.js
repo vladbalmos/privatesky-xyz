5f9d5f3c8e0a40050163bf6dc789b79ad1e23284
Paius Paraschiv-Catalin <p.cata95@gmail.com>
2020-01-16 16:43:15 +0200

    Merged new form components ++ Part of the form, form components documentation

--- cardinal/p-1zubqswr.system.entry.js
+++
@@ -1,1 +0,0 @@
-System.register(["./p-226ad382.system.js"],function(t){"use strict";var e,r;return{setters:[function(t){e=t.r;r=t.h}],execute:function(){var n=t("psk_label",function(){function t(t){e(this,t);this.label=null;this.for=null}t.prototype.render=function(){return r("label",{htmlFor:this.for},this.label)};return t}())}}});
