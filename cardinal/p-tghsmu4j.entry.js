b001d59a0b411103d7f6ea276d912ae9b600ea5f
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2020-03-05 13:05:59 +0200

    added psk-app-root documentation

--- cardinal/p-tghsmu4j.entry.js
+++
@@ -1,1 +0,0 @@
-import{r as s,h as r}from"./p-bd5842d7.js";const t=class{constructor(r){s(this,r)}render(){return r("psk-chapter",{title:this.title},r("div",{class:"sub-card"},r("slot",null)))}};export{t as psk_hoc};
