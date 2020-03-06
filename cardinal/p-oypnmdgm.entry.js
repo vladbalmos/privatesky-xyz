236616af889e90a313d0eb1c48954574fe7d3b17
Paius Paraschiv-Catalin <p.cata95@gmail.com>
2020-03-06 12:04:27 +0200

    moved how to create a cardinal site page into Cardinal root menu + fixed psk-list issue + refactor psk-form (removed default submit action in case form-actions is not defined)

---
+++ cardinal/p-oypnmdgm.entry.js
@@ -0,0 +1,1 @@
+import{r as o}from"./p-bd5842d7.js";import"./p-11ec80fb.js";import{i as s}from"./p-c2b42fe0.js";import"./p-4bed3927.js";const t=class{constructor(s){o(this,s)}componentWillLoad(){this.url?this.history.push(this.url,{}):console.error("Url was not provided")}};s(t);export{t as psk_route_redirect};
